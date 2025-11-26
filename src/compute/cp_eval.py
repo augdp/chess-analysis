2da066e160f36afd907abf98d0a2c84e7038eb87
augdp <augusto.dp@hotmail.com>
2025-11-25 22:16:18 -0300

    idk sry

--- src/compute/cp_eval.py
+++ src/compute/cp_eval.py
@@ -2,6 +2,9 @@
 
 class CPEvaluator:
     def __init__(self, engine_handle, depth=18, multipv=1):
+        pass
     def evaluate(self, board):
+        pass
         # returns { "eval_cp": x, "eval_mate": y }
     def _call_engine(self, board):
+        pass
